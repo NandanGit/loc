53a55296e51c6dc37cf67c3e20e5bc2709e47aef
Nandan Reddy <nandan.reddy1133@gmail.com>
2024-03-02 13:57:20 +0530

    Update Loc.ts and genTransTypes.ts
    Added cases where placeholders are not provided. In such cases, instead of a function, it returns a string.

--- playground/src/types/Loc.ts
+++ playground/src/types/Loc.ts
@@ -1,31 +1,37 @@
 export interface Loc {
   /** 
    * English
+   * No description provided
    */
   headerEnglish: string;
 
   /** 
    * Spanish
+   * No description provided
    */
   headerSpanish: string;
 
   /** 
    * French
+   * No description provided
    */
   headerFrench: string;
 
   /** 
    * Learn React
+   * No description provided
    */
   bodyLearnReact: string;
 
   /** 
    * Edit {filename} and save to reload.
+   * No description provided
    */
   bodyEditAndSave: (filename: string) => string;
 
   /** 
    * Know More
+   * No description provided
    */
   bodyKnowMore: string;
 }
